d9f74ec2f2ed9e9fab78d020ce8bcd6c22270376
Jaime Jiménez López <jjimenezlopez21@gmail.com>
2023-04-19 15:51:55 +0200

    Nodes map persistance

--- TFG_Lost_Time_Memory/Assets/Scripts/NodeMap/MapNode.cs
+++ TFG_Lost_Time_Memory/Assets/Scripts/NodeMap/MapNode.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class MapNode : MonoBehaviour
 {
     public int id, type;
+    public bool selected;
     int nodeSelected;
     TextMeshProUGUI btnTMP;
 
@@ -23,16 +24,20 @@ public class MapNode : MonoBehaviour
         if (PlayerPrefs.HasKey("nodeSelected" + id))
         {
             nodeSelected = PlayerPrefs.GetInt("nodeSelected" + id);
+            Debug.Log("Has key  nodeSelected " + id + ": " + nodeSelected);
         }
         else
         {
             nodeSelected = -1;
             SetNodeSelected(-1);
+            Debug.Log("Has no key  nodeSelected " + id + ": " + nodeSelected);
         }
+        SetButton();
     }
 
     public void SelectNode()
     {
+        selected = true;
         SetNodeSelected(1);
         SetTypeVars();
         ColorBlock cb = btn.colors;
@@ -41,7 +46,10 @@ public class MapNode : MonoBehaviour
         for(int i = 0; i < parentColumn.transform.childCount; i++)
         {
             parentColumn.transform.GetChild(i).GetComponent<Button>().interactable = false;
-            parentColumn.transform.GetChild(i).GetComponent<MapNode>().SetNodeSelected(0);
+            if(parentColumn.transform.GetChild(i).GetComponent<MapNode>().selected != true)
+            {
+                parentColumn.transform.GetChild(i).GetComponent<MapNode>().SetNodeSelected(0);
+            }
         }
     }
 
@@ -49,6 +57,28 @@ public class MapNode : MonoBehaviour
     {
         nodeSelected = mode;
         PlayerPrefs.SetInt("nodeSelected" + id, nodeSelected);
+        Debug.Log("nodeSelected " + id + ": " + nodeSelected);
+    }
+
+    void SetButton()
+    {
+        switch(nodeSelected)
+        {
+            case -1:
+                break;
+            case 0:
+                gameObject.GetComponent<Button>().interactable = false;
+                break;
+            case 1:
+                SetTypeVars();
+                ColorBlock cb = btn.colors;
+                cb.disabledColor = new Color(0, 1, 0, .5f);
+                btn.colors = cb;
+                gameObject.GetComponent<Button>().interactable = false;
+                break;
+            default:
+                break;
+        }
     }
 
     void SetTypeVars()
