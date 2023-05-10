9d06efdb952613b88d9dbcc6436e2ec0e6fc41a0
Jaime Jiménez López <jjimenezlopez21@gmail.com>
2023-05-10 13:05:00 +0200

    Lua function, my dialogue manager

--- TFG_Lost_Time_Memory/Assets/Scripts/Dialogues/MyDialogueManager.cs
+++ TFG_Lost_Time_Memory/Assets/Scripts/Dialogues/MyDialogueManager.cs
@@ -2,6 +2,7 @@ using PixelCrushers.DialogueSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MyDialogueManager : MonoBehaviour
 {
@@ -9,11 +10,41 @@ public class MyDialogueManager : MonoBehaviour
 
     public DialogueSystemTrigger dst;
 
+    [Tooltip("Typically leave unticked so temporary Dialogue Managers don't unregister your functions.")]
+    public bool unregisterOnDisable = false;
+
     void Start()
     {
         converName = GameManager.inst.converName;
 
         dst.conversation = converName;
+    }
+
+    void OnEnable()
+    {
+        Lua.RegisterFunction("ChangeToScene", this, SymbolExtensions.GetMethodInfo(() => ChangeToScene(string.Empty)));
+    }
+
+    void OnDisable()
+    {
+        if (unregisterOnDisable)
+        {
+            Lua.UnregisterFunction("ChangeToScene");
+        }
+    }
+
+    public void DebugLog(string message)
+    {
+        Debug.Log(message);
+    }
+
+    public double AddOne(double value)
+    { // Note: Lua always passes numbers as doubles.
+        return value + 1;
+    }
+    public void ChangeToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 
 
